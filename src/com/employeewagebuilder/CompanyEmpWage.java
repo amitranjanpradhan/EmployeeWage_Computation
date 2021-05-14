3a7cb8688d4ae1b820f92cb6678c0a683c3a4535
amitranjan <amitranjanpradhan456@gmail.com>
2021-05-14 07:14:22 +0530

    [Amit] Added Welcome Message

---
+++ src/com/employeewagebuilder/CompanyEmpWage.java
@@ -0,0 +1,7 @@
+package com.employeewagebuilder;
+
+public class CompanyEmpWage {
+public static void main(String[] args) {
+	System.out.println("Welcome To EmployeeWage Computation Program");
+}
+}
